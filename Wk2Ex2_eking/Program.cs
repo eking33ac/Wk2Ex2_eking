86207074938343108ea0b3fff2b2cd8c13e941c5
eking33ac <eking33@amarillocollege.com>
2025-01-26 09:23:29 -0600

    Update Program.cs
    Put input statements and readlines on the same lines. Added a blank line to visually separate the input area from the results area.

--- Wk2Ex2_eking/Program.cs
+++ Wk2Ex2_eking/Program.cs
@@ -16,36 +16,38 @@ namespace Wk2Ex2_eking
             double width = 0;
             double area = 0;
             double perimeter = 0;
-
-
+            
+            
             // Output statement prompting user to input the length of a rectangle
-            Console.WriteLine("Enter the length of the rectangle: ");
+            Console.Write("Enter the length of the rectangle: ");
             // Collect user input for length of rectangle
             length = double.Parse(Console.ReadLine());
-
+            
             // Output statement prompting user to input the width of a rectangle
-            Console.WriteLine("Enter the width of the rectangle: ");
+            Console.Write("Enter the width of the rectangle: ");
             // Collect user input for width of rectangle
             width = double.Parse(Console.ReadLine());
-
-
+            
+            
             // Calculate area of rectangle
             area = length * width;
-
+            
             // Calculate perimeter of rectangle
             perimeter = (length * 2) + (width * 2);
-
-
+            
+            // Have a blank line between inputs and results
+            Console.WriteLine();
+            
             // Output statement for Results Title
             Console.WriteLine("Results:");
-
+            
             // Output statement for  Area of rectangle
             Console.WriteLine($"Area of the rectangle: {area}");
-
+            
             // Output statement for perimeter of rectangle
             Console.WriteLine($"Perimeter of the rectangle: {perimeter}");
-
-
+            
+            
             // Pause screen for user to read output
             Console.ReadLine();
         }
